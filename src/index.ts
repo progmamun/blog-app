a2956dd58dbfa76c48eaf2eda4a1f562786687b2
progmamun <mamun333089@gmail.com>
2023-11-15 00:25:31 +0600

    file-and-folder-design

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "../src/schema";
 import { resolvers } from "../src/resolvers";
 
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+const prisma = new PrismaClient();
+
+interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -11,6 +20,9 @@ const server = new ApolloServer({
 const main = async () => {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async (): Promise<Context> => {
+      return { prisma };
+    },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
